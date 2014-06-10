3e886f75bb88126d86dd1b351783b24c689a991f
Demirhan Aydin <demirhanaydin@gmail.com>
2014-06-10 15:05:01 +0300

    added model validation to place

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,6 +2,8 @@ class Place < ActiveRecord::Base
   reverse_geocoded_by :latitude, :longitude
   after_validation :reverse_geocode, :if => :latitude_changed?
 
+  validates :title, presence: true
+
   def latlng
     [latitude, longitude]
   end
