0f193a438368fcaa80bb896a44242641de2825ac
Demirhan Aydin <demirhanaydin@gmail.com>
2014-09-18 13:41:45 +1000

    updated api key

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -6,7 +6,7 @@
 // }
 $(document).ready(function(){
   var map = L.map('map').setView([41.08522, 29.04672], 15);
-      L.tileLayer('http://{s}.tile.cloudmade.com/8ee2a50541944fb9bcedded5165f09d9/997/256/{z}/{x}/{y}.png', {
+      L.tileLayer('http://{s}.tile.cloudmade.com/'+window.cloudmade_api_key+'/997/256/{z}/{x}/{y}.png', {
           attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
           maxZoom: 18
       }).addTo(map);
